f5156417a3894f0166a1e3556146cb01e12fc8b1
Ryan Palumbo <rynpal@gmail.com>
2022-06-09 16:33:23 -0400

    add jquery

---
+++ index.js
@@ -0,0 +1,10 @@
+const rock = "🗿";
+const paper = "📄";
+const scissors = "✂";
+
+const shooters = $(".shooter");
+const countdown = $("#countdown");
+
+shooters.click(() => {
+  console.log("shooter clicked");
+});
