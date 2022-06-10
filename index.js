69372a8969bc54ed07afa261a3f8173af7f49e14
Ryan Palumbo <rynpal@gmail.com>
2022-06-10 16:11:43 -0400

    fade shooter buttons cycle through game text

--- index.js
+++ index.js
@@ -2,9 +2,43 @@ const rock = "🗿";
 const paper = "📄";
 const scissors = "✂";
 
+const responses = [rock, paper, scissors];
+
 const shooters = $(".shooter");
 const countdown = $("#countdown");
 
+function respond(arr) {
+  return arr[Math.floor(Math.random() * arr.length)];
+}
+
+function fadeShooters(shooterEl) {
+  shooterEl.fadeOut(1000);
+}
+
+function changeText(cd, str) {
+  cd.text(str);
+}
+
+function cycleText(cd) {
+  setTimeout(() => {
+    changeText(cd, "ROCK");
+    setTimeout(() => {
+      changeText(cd, "PAPER");
+      setTimeout(() => {
+        changeText(cd, "SCISSORS");
+        setTimeout(() => {
+          changeText(cd, "SHOOT!");
+          setTimeout(() => {
+            cd.toggle();
+          }, 400);
+        }, 400);
+      }, 400);
+    }, 400);
+  }, 1100);
+}
+
 shooters.click(() => {
-  console.log("shooter clicked");
+  fadeShooters(shooters);
+  countdown.toggle();
+  cycleText(countdown);
 });
