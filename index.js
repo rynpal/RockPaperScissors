aaf8e5b81a5d56b3d4b5cc57f650cd29e3a1bfbc
Ryan Palumbo <rynpal@gmail.com>
2022-06-12 15:25:27 -0400

    change restart to button add result text cycle and restart button appearance change button appearance

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const responses = [rock, paper, scissors];
 
 const shooters = $(".shooter");
 const countdown = $("#countdown");
+const restart = $("#restart");
 let choice = null;
 let response = null;
 
@@ -21,7 +22,25 @@ function changeText(cd, str) {
   cd.text(str);
 }
 
-function cycleText(cd) {
+function displayResults(cd, c, r) {
+  cd.text(c + " vs. " + r);
+}
+
+function resultText(c, r) {
+  if (c == rock) {
+    console.log("You picked rock");
+  } else if (c == paper) {
+    console.log("You picked paper");
+  } else {
+    console.log("You picked scissors");
+  }
+}
+
+function playAgain() {
+  console.log("clicked play again");
+}
+
+function cycleText(cd, c, r) {
   setTimeout(() => {
     changeText(cd, "ROCK");
     setTimeout(() => {
@@ -31,7 +50,13 @@ function cycleText(cd) {
         setTimeout(() => {
           changeText(cd, "SHOOT!");
           setTimeout(() => {
-            cd.toggle();
+            displayResults(cd, c, r);
+            setTimeout(() => {
+              resultText(c, r);
+              setTimeout(() => {
+                restart.toggle();
+              }, 400);
+            }, 200);
           }, 400);
         }, 400);
       }, 400);
@@ -42,9 +67,15 @@ function cycleText(cd) {
 function runGame() {
   fadeShooters(shooters);
   countdown.toggle();
-  cycleText(countdown);
+  cycleText(countdown, choice, response);
 }
 
-shooters.click(() => {
+shooters.click((e) => {
+  choice = $(e.target).text();
+  response = respond(responses);
   runGame();
+});
+
+restart.click(() => {
+  playAgain();
 });
